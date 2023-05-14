66564409552e744e984ac987baa7fbf5076dc372
Alexandrelimax <alexandrelimaxxxx@gmail.com>
2023-05-14 20:08:09 -0300

    Primeiro commit

---
+++ src/Cronometro.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Cronometro implements Runnable{
+private JTextField jTextField;
+
+    public Cronometro(JTextField jTextField) {
+        this.jTextField = jTextField;
+
+    }
+        @Override
+        public void run() {
+
+            while(true){
+
+                jTextField.setText(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").
+                        format(Calendar.getInstance().getTime()));
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+    }
+
---
+++ src/Painel.java
@@ -0,0 +1,76 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Painel extends JDialog {
+
+    private JPanel jPanel = new JPanel(new GridBagLayout());
+    private JTextField jTextField = new JTextField();
+    private JLabel jLabel = new JLabel("Time");
+    private JButton startButton = new JButton("Start");
+    private JButton stopButton = new JButton("Stop");
+    private Thread threadTime;
+
+
+    public Painel() {
+
+        setTitle("Cronometro");
+        setResizable(false);
+        setSize(new Dimension(200,240));
+        setLocationRelativeTo(null);
+
+
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.insets = new Insets(3,14,5,5);
+
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+
+        jLabel.setPreferredSize(new Dimension(30,25));
+        jPanel.add(jLabel,gridBagConstraints);
+
+        jTextField.setPreferredSize(new Dimension(128,25));
+        jTextField.setEnabled(false);
+        gridBagConstraints.gridy++;
+        jPanel.add(jTextField,gridBagConstraints);
+
+
+        gridBagConstraints.gridwidth = 1;
+        startButton.setPreferredSize(new Dimension(70,25));
+        gridBagConstraints.gridy++;
+        jPanel.add(startButton,gridBagConstraints);
+
+        gridBagConstraints.gridwidth = 1;
+        stopButton.setPreferredSize(new Dimension(70,25));
+        gridBagConstraints.gridx++;
+        jPanel.add(stopButton,gridBagConstraints);
+        stopButton.setEnabled(false);
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                threadTime = new Thread(new Cronometro(jTextField));
+                threadTime.start();
+                stopButton.setEnabled(true);
+                startButton.setEnabled(false);
+
+
+            }
+        });
+
+        stopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                threadTime.stop();
+                startButton.setEnabled(true);
+                stopButton.setEnabled(false);
+            }
+        });
+
+        add(jPanel, BorderLayout.WEST);
+
+        setVisible(true);
+    }
+}
